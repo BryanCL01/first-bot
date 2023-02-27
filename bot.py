ee38cdb65e5bab281c021bf2f76dd9ea507b9bcf
root <55458737+BryanCL01@users.noreply.github.com>
2023-02-26 23:51:47 -0800

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTA3OTY2MzAxMTQ3Nzg2MDQ0Mw.GCfIwQ.t1oh1fbrVrcXy3WcYcYerkKR_V93RkPjRCKGwc'
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is no running!')
+
+    client.run(TOKEN)
